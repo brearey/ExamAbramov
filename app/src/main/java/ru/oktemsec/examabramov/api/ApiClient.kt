f0ee55edfc8d2dc31042e492bfd093eafac2ae72
Brearey <brearey4@gmail.com>
2021-12-03 14:23:43 +0900

    Realized ApiClient.kt with login features

--- app/src/main/java/ru/oktemsec/examabramov/api/ApiClient.kt
+++ app/src/main/java/ru/oktemsec/examabramov/api/ApiClient.kt
@@ -1,5 +1,34 @@
 package ru.oktemsec.examabramov.api
 
-class ApiClient {
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.oktemsec.examabramov.interfaces.LoginService
 
+class ApiClient {
+    private fun getRetrofit(): Retrofit {
+        val httpLoggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient: OkHttpClient = OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+        val gson: Gson = GsonBuilder()
+            .setLenient()
+            .create()
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl("http://cinema.areas.su")
+            .build()
+    }
+
+    fun getLogin(): LoginService {
+        return getRetrofit().create(LoginService::class.java)
+    }
 }
